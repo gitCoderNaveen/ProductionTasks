5300d75846a7bb336518ea0fb0163201bd53e165
JAVAVEEN <naveenbsc.mca1518@gmail.com>
2024-08-10 05:35:07 +0530

    deploy changes

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,13 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin:['http://deploy-mern-1whq.vercel.app'],
+    methods:['POST','GET'],
+    credentials:true
+}))
 
-mongoose.connect('mongodb://127.0.0.1:27017/ProductDirectory')
+mongoose.connect('mongodb+srv://naveenbscmca1518:product@2024@cluster0.cfshq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 mongoose.connection.on('connected', ()=>{
     console.log('MongoDB successfully connectd')
